612fe71fb64128870c2ce2321f82441b5c6e37bf
Yevhenii Chuprun <chuprun.ievgenii@gmail.com>
2022-06-19 13:53:14 +0200

    Quiz - done

--- 54-55-quiz/before/script.js
+++ 54-55-quiz/before/script.js
@@ -1,16 +1,40 @@
-/*
-  TODO: 2. Select all elements needed
-    * The form element (has the id `quiz-form`)
-    * The answer inputs (have the class `answer`)
-    * BONUS: The questions (have the class `question-item`)
-    * BONUS: The alert (has the id `alert`)
-*/
+const form = document.querySelector('#quiz-form');
+const answers = Array.from(document.querySelectorAll('.answer'));
+const questions = Array.from(document.querySelectorAll('.question-item'));
+const alert = document.querySelector('#alert');
 
-// TODO: 3. Create a submit event listener for the form that does the following.
-//    1. Prevent the default behaviour
-//    2. Get all selected answers (use the `checked` property on the input to determine if it is selected or not)
-//    3. Loop through the selected answer to see if they are correct or not (Check the value of the answer to see if it is the string "true")
-//    4. For each correct answer add the class `correct` to the parent with the class `question-item` and remove the class `incorrect`.
-//    5. For each incorrect answer add the class `incorrect` to the parent with the class `question-item` and remove the class `correct`.
-//    6. BONUS: Make sure unanswered questions show up as incorrect. The easiest way to do this is to add the incorrect class and removing the correct class from all question items before checking the correct answers
-//    7. BONUS: If all answers are correct show the element with the id `alert` and hide it after one second (look into setTimeout) (use the class active to show the alert and remove the class to hide it)
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  
+  questions.forEach(questionItem => {
+    questionItem.classList.add('incorrect')
+    questionItem.classList.remove('correct')
+  });
+
+  const filtered = answers.filter(a => a.checked);
+  
+  filtered.forEach(x => updateSelection(x));
+  
+  const isAllCorrect = !questions.some(q => q.classList.contains('incorrect'));
+  const allAnswered = filtered.length === questions.length
+
+  if(isAllCorrect && allAnswered) {
+    alert.classList.add('active');
+    setTimeout(() => {
+      alert.classList.remove('active');
+    }, 1000);
+  }
+});
+
+function updateSelection(element) {
+  const value = element.value
+  const parent = element.closest('.question-item');
+  
+  if(value === 'true') {
+    parent.classList.remove('incorrect')
+    parent.classList.add('correct')
+   } else {
+    parent.classList.remove('correct')
+    parent.classList.add('incorrect')
+   }
+}
